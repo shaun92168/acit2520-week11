fc923c33887821856c7e13b3a4e7808b77367405
Mu Hsuan Tseng <mtseng6@my.bcit.ca>
2018-11-13 13:59:20 -0800

    first commit: add all files to git

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('message', (text) => {
+	return text.toUpperCase();
+});
+
+app.use((request, response, next) => {
+	var time = new Date().toString();
+	// console.log(`${time}: ${request.method} ${request.url}`);
+	var log = `${time}: ${request.method} ${request.url}`;
+	fs.appendFile('server.log', log + '\n', (error) => {
+		if (error) {
+			console.log('Unable to log message');
+		}
+	});
+
+	response.render('maintenance.hbs', {
+		title: 'Maintenance'
+	});
+
+	//next();
+});
+
+app.get('/', (request, response) => {
+	// response.send('<h1>Hello Express!</h1>');
+	response.send({
+		name: 'Your Name',
+		school: [
+			'BCIT',
+			'SFU',
+			'UBC'
+		]
+	})
+});
+
+app.get('/info', (request, response) => {
+	response.render('about.hbs', {
+		title: 'About page',
+		year: new Date().getFullYear(),
+		menuOne: 'Home',
+		menuTwo: 'About',
+		menuThree: 'Info',
+		linkOne: '/',
+		linkTwo: '/shaun',
+		linkThree: '/info',
+		welcome: 'Hello!'
+	});
+});
+
+app.get('/shaun', (request, response) => {
+	response.render('shaun.hbs', {
+		title: 'Shaun',
+		year: new Date().getFullYear(),
+		welcome: 'nice to meet you!',
+		menu:[
+		{name:'Home', link:'/'},
+		{name:'Shaun', link:'/shaun'},
+		{name:'Info', link:'/info'}
+		],
+		myImage: 'https://www.bankofcanada.ca/wp-content/uploads/2017/02/c150-carousel-image-500x250.jpg'
+	});
+});
+
+app.get('/404', (request, response) => {
+	response.send({
+		error: 'Page not found'
+	})
+})
+
+app.listen(8080, () => {
+	console.log('Server is up on the port 8080');
+});
